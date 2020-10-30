d531def6cce4e7fd758a169387e9e8ebbe83b8a0
Ludwik Rydzak <ludwik.rydzak@gmail.com>
2020-10-30 03:10:21 +0100

    Gotowe wczytywanie mnista. Działa forward propagation.

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from mlp import *
+from mnist import MNIST
+mndata = MNIST('mnist')
+images, labels = mndata.load_training()
 wagi = [5,6,4,6]
 wagirange = [-1,1]
 biasrange = [-1,1]
-network = mlp(wagi, wagirange , biasrange, 1)
+break_rule = 0
+network = mlp(wagi, wagirange , biasrange, [1,2,2], break_rule)
 network.display_mlp()
+print('------')
+print(network.forward_propagation([5,4,3,2,1]))
+
+print(mndata.display(images[0]))
+print(labels[0])
--- mlp.py
+++ mlp.py
@@ -1,18 +1,22 @@
 import math
 import random
-
 import numpy as np
+
+
 class mlp:
-    def __init__(self, _layers_size, _weight_range, _bias_range, _activation_functions ):
+    def __init__(self, _layers_size, _weight_range, _bias_range, _activation_functions, _break_rule):
         self.layers_size = _layers_size #rozmiary warstw, rozmiar wejśicia podawany jest jako zerowa warstwa
         self.activation_functions = _activation_functions
+        self.break_rule = _break_rule
         self.weights = []
         self.biases = []
-        for l in range(len(_layers_size)-1):#liczba macierzy
-            self.weights.append([[(self.get_random_from_range(_weight_range))for col in range(_layers_size[l])] for row in range(_layers_size[l+1])])
-        self.biases.append(self.get_random_from_range(_weight_range))
         self.pobudzenia = []
         self.wyjscia = []
+        for l in range(len(_layers_size)-1):#liczba macierzy
+            self.weights.append([[(self.get_random_from_range(_weight_range))for col in range(_layers_size[l])] for row in range(_layers_size[l+1])])
+            self.biases.append(self.get_random_from_range(_weight_range))
+            self.pobudzenia.append([])
+            self.wyjscia.append([])
 
     def display_mlp(self):
        for i in self.weights:
@@ -23,7 +27,8 @@ class mlp:
     def get_random_from_range(self, start_end):
         return (random.random() * (start_end[1]-start_end[0]) + start_end[0])
 
-    def softMax(self, x):
+    def softMax(self, _x):
+        x = np.array(_x)
         return x / sum(x)
 
     def activation_function(self, pobudzenie, layer):
@@ -32,7 +37,7 @@ class mlp:
             if(self.activation_functions[layer]==1):
                 ret.append(self.activation_linear(i))
             elif(self.activation_functions[layer]==2):
-                ret.append(self.activation_sigmoidal(i))
+                ret.append(self.activation_sigmoid(i))
             elif(self.activation_functions[layer]==3):
                 ret.append(self.activation_binary(i))
         return ret
@@ -45,7 +50,7 @@ class mlp:
         else:
             return pobudzenie
 
-    def activation_sigmoid(x):
+    def activation_sigmoid(self, x):
         return 1 / (1 + math.exp(-x))
     def activation_binary(self, pobudzenie):
         if(pobudzenie > 0):
@@ -57,7 +62,9 @@ class mlp:
         return np.dot(_weights, np.transpose(_values)) + _bias
 
     def forward_propagation(self, _entry_values):
+        entry_values = _entry_values
         for layer_number in range(len(self.layers_size)-1):
-            self.pobudzenia[layer_number] = self.pobudzenie(self.weights[layer_number], _entry_values, self.biases[layer_number])
+            self.pobudzenia[layer_number] = self.pobudzenie(self.weights[layer_number], entry_values, self.biases[layer_number])
             self.wyjscia[layer_number]= self.activation_function(self.pobudzenia[layer_number],layer_number)
-        return self.softMax(self.wyjscia[len(self.layers_size)-1])
+            entry_values = self.wyjscia[layer_number]
+        return self.softMax(self.wyjscia[len(self.layers_size)-2])
