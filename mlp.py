182378570ddb360abcc08ce2a219c3d697b2c502
Ludwik Rydzak <ludwik.rydzak@gmail.com>
2020-11-05 03:19:36 +0100

    dodany tangens hiperboliczny i pochodna softmaxa

--- mlp.py
+++ mlp.py
@@ -27,10 +27,6 @@ class mlp:
     def get_random_from_range(self, start_end):
         return (random.random() * (start_end[1]-start_end[0]) + start_end[0])
 
-    def softMax(self, _x):
-        x = np.array(_x)
-        return x / sum(x)
-
     def activation_function(self, pobudzenie, layer):
         ret = []
         for i in pobudzenie:
@@ -39,32 +35,50 @@ class mlp:
             elif(self.activation_functions[layer]==2):
                 ret.append(self.activation_sigmoid(i))
             elif(self.activation_functions[layer]==3):
-                ret.append(self.activation_binary(i))
+                ret.append(self.activation_tanh(i))
         return ret
 
-    def activation_linear(self, pobudzenie):
-        if(pobudzenie >1):
-            return 1
-        elif(pobudzenie <0):
-            return 0
-        else:
-            return pobudzenie
+    def activation_linear(self, x):
+            return x
+    def derivative_linear(self,x):
+        return 1
+
+    def activation_tanh(self, x):
+        return (math.exp(x)-math.exp(-x))/(math.exp(x)+math.exp(-x))
+    def derivative_tanh(self, x):
+        return 1-self.activation_tanh(x)**2
 
     def activation_sigmoid(self, x):
         return 1 / (1 + math.exp(-x))
-    def activation_binary(self, pobudzenie):
-        if(pobudzenie > 0):
-            return 1
-        else:
-            return 0
+    def derivative_sigmoid(self, x):
+        sig = self.activation_sigmoid(x)
+        return sig*(1-sig)
+
+    def softMax(self, _x):
+        sum=0
+        for x in _x:
+            sum+=np.exp(x)
+        return np.exp(_x) / sum
+    def derivative_softmax(self,s):#https://stackoverflow.com/questions/54976533/derivative-of-softmax-function-in-python
+        jacobian_m = np.diag(s)#inicjuję macierz
+        for i in range(len(jacobian_m)):
+            for j in range(len(jacobian_m)):
+                if i == j:
+                    jacobian_m[i][j] = s[i] * (1 - s[i])#jesli i=j, to jeden wzorek na pochodna z wykladu
+                else:
+                    jacobian_m[i][j] = -s[i] * s[j]#jesli nie, to drugi wzorek na pochodną z wykladu
+        return jacobian_m
+
 
     def pobudzenie(self, _weights, _values, _bias):
         return np.dot(_weights, np.transpose(_values)) + _bias
 
     def forward_propagation(self, _entry_values):
         entry_values = _entry_values
-        for layer_number in range(len(self.layers_size)-1):
+        for layer_number in range(len(self.layers_size)-2):
             self.pobudzenia[layer_number] = self.pobudzenie(self.weights[layer_number], entry_values, self.biases[layer_number])
             self.wyjscia[layer_number]= self.activation_function(self.pobudzenia[layer_number],layer_number)
             entry_values = self.wyjscia[layer_number]
+        self.pobudzenia[len(self.layers_size)-2] = self.pobudzenie(self.weights[len(self.layers_size)-2], entry_values,self.biases[len(self.layers_size)-2])
+        self.wyjscia[len(self.layers_size)-2] = self.pobudzenia[len(self.layers_size)-2]#ostatnia warstwa bez funkcji aktywacji
         return self.softMax(self.wyjscia[len(self.layers_size)-2])
