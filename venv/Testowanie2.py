51b76b66c0c62b4039805a3535fb86d3c7725f87
Ludwik Rydzak <ludwik.rydzak@gmail.com>
2020-11-22 20:26:24 +0100

    funkcja testująca dla momentum

--- venv/Testowanie2.py
+++ venv/Testowanie2.py
@@ -9,27 +9,27 @@ class Testowanie2:
         self.weights_ranges = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,2,5,10]
         self.learning_factors = [0.0001,0.0005,0.001,0.005,0.01,0.02,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,1.5,2]
         self.activation_functions =[1,2,3,4]
-        self.break_rule = 5
+        self.break_rule = 3
         self.biasrange = [-1,1]
         self.x_epoki = []
         self.x_procenty = []
         self.rozmiary_sieci = [784,70,10]
-        self.wspolczynnik_momentum = [0.9]
+        self.wspolczynnik_momentum = [0,0.8,0.81,0.82,0.83,0.84,0.85,0.86,0.87,0.88,0.89,0.9]
         self.wspolczynnik_momentum2 = [0.99]
         self.opcja_momentum = [1,2,3,4,5]
 
-    def test_hidden_layer(self, train_set, train_labels, test_set, test_labels):
-        with open('badania_hidden_size.txt', 'w') as file:
-            file.writelines('liczba neuronów warstwy ukrytej;liczba epok;poprawne_odpowiedzi\n')
-            for size in self.hidden_layer_sizes:
+    def test_momentum(self, train_set, train_labels, test_set, test_labels):
+        with open('badania_momentum.txt', 'w') as file:
+            file.writelines('wspolczynnik momentum;liczba epok;poprawne_odpowiedzi\n')
+            for wspolczynnik_momentum in self.wspolczynnik_momentum:
                 suma_procentów = 0
                 suma_epok = 0
-                print(f'rozmiar ukrytej: {size}')
+                print(f'wspolczynnnik momentum: {size}')
                 for iteracja in range(10):
-                    #         rozmiar macierzy  wagi_range         sigmoid        współczynnik uczenia
-                    network = mlp([784, size, 10], [-1, 1], self.biasrange, [2], self.break_rule, 0.1)
+
+                    network = mlp([784, 70, 10], [-1, 1], self.biasrange, [2], self.break_rule, 0.1, wspolczynnik_momentum, self.wspolczynnik_momentum2)
                     liczba_epok = network.uczenie_calosc(train_set, train_labels, epoki=500, batch_size=32)
-                    print(f'iteracja: {iteracja} badana zmianna:{size} liczba epok: {liczba_epok}\n')
+                    print(f'iteracja: {iteracja} badana zmianna:{wspolczynnik_momentum} liczba epok: {liczba_epok}\n')
                     np.set_printoptions(suppress=True)
                     procent, macierz_odpowiedzi = network.validate(test_set, test_labels)
                     suma_epok += liczba_epok
@@ -37,20 +37,20 @@ class Testowanie2:
                 self.x_epoki.append(suma_epok / 10)
                 self.x_procenty.append(suma_procentów / 10)
                 file.writelines(f'{size};{suma_epok / 10};{suma_procentów / 10}\n')
-            self.show_hidden_size_procent(self.hidden_layer_sizes, self.x_procenty)
-            self.show_hidden_size_epoki(self.hidden_layer_sizes, self.x_epoki)
+            self.show_momentum_procent(self.hidden_layer_sizes, self.x_procenty)
+            self.show_momentum_epoki(self.hidden_layer_sizes, self.x_epoki)
 
     def show_weights_range_epoki(self,x, y):
-        plt.title('Badanie wpływu momentum na uczenie')
-        plt.xlabel('zakres wag')
+        plt.title('Badanie wpływu wspolczynnika momentum dla momentum na uczenie')
+        plt.xlabel('wpsolczynnik momentum')
         plt.ylabel('epoki')
         plt.plot(x, y, 'bo', label='średnia liczba epok')
         plt.legend()
         plt.show()
 
     def show_weights_range_epoki(self,x, y):
-        plt.title('Badanie wpływu zakresu wag na szybkość uczenia')
-        plt.xlabel('zakres wag')
+        plt.title('Badanie wpływu wspolczynnika momentum dla momentum na szybkość uczenia')
+        plt.xlabel('wspolczynnik momentum')
         plt.ylabel('epoki')
         plt.plot(x, y, 'bo', label='średnia liczba epok')
         plt.legend()
